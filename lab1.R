74bdb904ec65a40f817fc6695c12ce039df45b71
mronethere <mronethere@gmail.com>
2015-11-10 23:08:13 +0200

    add lab1

--- lab1.R
+++ lab1.R
@@ -1,1 +1,12 @@
-1+1
+library(ggplot2)
+stats <- read.csv("./data/lab1.csv")
+counts <- as.numeric(stats$count)
+temp <- table(as.vector(counts))
+
+avrg <- mean(counts)
+mode <- names(temp)[temp == max(temp)]
+mdn <- median(counts)
+dsp <- var(counts)
+acf(counts)
+
+ggplot(data=stats, aes(x=year, y=count)) + geom_line() + geom_point()
